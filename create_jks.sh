1404a3887253bfcc081d67fb77fb7708703becbf
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2023-11-18 15:11:19 +0300

    automate JKS creation

--- create_jks.sh
+++ create_jks.sh
@@ -3,35 +3,60 @@
 # generate CA key and root certificate
 # using create_ca.sh
 
-# import CA root certificate to trustsore server and client truststore
-keytool -keystore kafka.server.truststore.jks -alias CARoot -import -file root.pem 
-keytool -keystore kafka.client.truststore.jks -alias CARoot -import -file root.pem
+ROOT=root
+NAME=${1}
+HOSTNAME=${2}
+
+[[ -z "${NAME}" || -z "${HOSTNAME}" ]] && {
+    echo "Usage: $0 <name> <hostname>"
+    echo "example: $0 client example.org"
+    exit
+}
+
+TRUSTSTORE_PASSWORD=$(head -c 32 /dev/random | base64 | head -c 10)
+echo $TRUSTSTORE_PASSWORD | cat > ${NAME}.truststore.pas
+echo "Generating keystore password to ${NAME}.keystore.pas ..."
+
+KEYSTORE_PASSWORD=$(head -c 32 /dev/random | base64 | head -c 10)
+echo $KEYSTORE_PASSWORD | cat > ${NAME}.keystore.pas
+echo "Generating keystore password to ${NAME}.keystore.pas ..."
+
+KEY_PASSWORD=$(head -c 32 /dev/random | base64 | head -c 10)
+echo $KEY_PASSWORD | cat > ${NAME}.key.pas
+echo "Generating key password to ${NAME}.key.pas ..."
+
+# import CA root certificate to trustsore
+keytool -import -noprompt -keystore ${NAME}.truststore.jks \
+    -storepass "${TRUSTSTORE_PASSWORD}" -storetype JKS \
+    -alias CARoot -file root.pem \
+    -ext SAN:DNS:${HOSTNAME} \
+    2>/dev/null
 
 # --------------- server --------------------
 
-# generate server key and certificate request
-keytool -keystore kafka.server.keystore.jks -alias localhost -validity 365 -genkey -keyalg RSA
-keytool -keystore kafka.server.keystore.jks -alias localhost -certreq -file kafka.server.req
+# generate key
+keytool -genkey -noprompt -keystore ${NAME}.keystore.jks \
+    -storepass ${KEYSTORE_PASSWORD} -keypass ${KEY_PASSWORD} -storetype JKS \
+    -alias localhost -keysize 2048 -validity 825 -keyalg RSA \
+    -dname "CN=${HOSTNAME}, OU=Dev, O=SBER, L=Moscow, ST=MSK, C=RU" \
+    2>/dev/null
+
+# generate certificate signing request
+keytool -certreq -noprompt -keystore ${NAME}.keystore.jks \
+    -storepass ${KEYSTORE_PASSWORD} -keypass ${KEY_PASSWORD} -storetype JKS \
+    -alias localhost -file ${NAME}.cert.req \
+    2>/dev/null
 
 # sign server certificate
-openssl x509 -req -CA root.pem -CAkey root.key -in kafka.server.req -out kafka.server.pem -days 365 -CAcreateserial 
+openssl x509 -req -CA root.pem -CAkey root.key -in ${NAME}.cert.req \
+    -passin file:${ROOT}.pas -out ${NAME}.cert.pem -days 825 -CAcreateserial
 
 # import CA root certificate signed certificate keystore to server keystore
-keytool -keystore kafka.server.keystore.jks -alias CARoot -import -file root.pem
-keytool -keystore kafka.server.keystore.jks -alias localhost -import -file kafka.server.pem
-
-
-# --------------- client --------------------
-
-# generate client key and certificate request
-keytool -keystore kafka.client.keystore.jks -alias localhost -validity 365 -genkey -keyalg RSA
-keytool -keystore kafka.client.keystore.jks -alias localhost -certreq -file kafka.client.req
-
-# sign client certificate
-openssl x509 -req -CA root.pem -CAkey root.key -in kafka.client.req -out kafka.client.pem -days 365 -CAcreateserial
-
-
-# импортируем CA серт и подписанный серт к keystore клиента1
-keytool -keystore kafka.client.keystore.jks -alias CARoot -import -file root.pem
-keytool -keystore kafka.client.keystore.jks -alias localhost -import -file kafka.client.pem
-
+keytool -import -noprompt -keystore ${NAME}.keystore.jks \
+    -storepass ${KEYSTORE_PASSWORD} -keypass ${KEY_PASSWORD} \
+    -storetype JKS -alias CARoot -file root.pem \
+    2>/dev/null
+keytool -import -noprompt -keystore ${NAME}.keystore.jks \
+    -storepass ${KEYSTORE_PASSWORD} -keypass ${KEY_PASSWORD} \
+    -storetype JKS -alias localhost -file ${NAME}.cert.pem \
+    2>/dev/null
